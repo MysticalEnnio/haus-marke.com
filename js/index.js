3f3651099415ee30a0b3382c39ae8c3ff8738bb3
Ennio Marke <mystical.ennio@gmail.com>
2023-01-31 14:38:58 -0600

    fetch sites

--- js/index.js
+++ js/index.js
@@ -1,63 +1,40 @@
-let websites = [
-  {
-    name: "Pizza",
-    url: "https://pizza.haus-marke.com",
-  },
-  {
-    name: "Brau",
-    url: "https://brau.haus-marke.com",
-  },
-  {
-    name: "Smart",
-    url: "https://smart.haus-marke.com",
-  },
-  {
-    name: "Aufgaben",
-    url: "https://aufgaben.haus-marke.com",
-  },
-  {
-    name: "Listen",
-    url: "https://listen.haus-marke.com",
-  },
-  {
-    name: "Kalender",
-    url: "https://kalender.haus-marke.com",
-  },
-];
+fetch("https://api.haus-marke.com/sites/getSites")
+  .then((res) => res.json())
+  .then((websites) => {
+    const websitesContainer = document.getElementById("websites");
 
-const websitesContainer = document.getElementById("websites");
-
-function loadWebsite(websiteOptions) {
-  const h2 = document.createElement("h2");
-  h2.innerText = websiteOptions.name.toUpperCase();
-  h2.dataset.url = websiteOptions.url;
-  websitesContainer.appendChild(h2);
-}
-
-loadWebsite({ name: "", url: "" });
-websites.forEach((website) => {
-  loadWebsite(website);
-});
-loadWebsite({ name: "", url: "" });
-
-document.getElementById("enterButton").addEventListener("click", () => {
-  console.log("Enter button clicked");
-  [].slice.call(websitesContainer.children).forEach(function (ele, index) {
-    console.log(`${
-      ele.innerHTML
-    } ${Math.abs(ele.getBoundingClientRect().top - websitesContainer.getBoundingClientRect().top - 300)}
-    `);
-    if (
-      Math.abs(
-        ele.getBoundingClientRect().top -
-          websitesContainer.getBoundingClientRect().top -
-          300
-      ) < 100
-    ) {
-      document.body.classList.add("fadeOutToLeft");
-      setTimeout(() => {
-        window.location.href = ele.dataset.url;
-      }, 500);
+    function loadWebsite(websiteOptions) {
+      const h2 = document.createElement("h2");
+      h2.innerText = websiteOptions.name.toUpperCase();
+      h2.dataset.url = websiteOptions.url;
+      websitesContainer.appendChild(h2);
     }
+
+    loadWebsite({ name: "", url: "" });
+    websites.forEach((website) => {
+      loadWebsite(website);
+    });
+    loadWebsite({ name: "", url: "" });
+
+    document.getElementById("enterButton").addEventListener("click", () => {
+      console.log("Enter button clicked");
+      [].slice.call(websitesContainer.children).forEach(function (ele, index) {
+        console.log(`${
+          ele.innerHTML
+        } ${Math.abs(ele.getBoundingClientRect().top - websitesContainer.getBoundingClientRect().top - 300)}
+    `);
+        if (
+          Math.abs(
+            ele.getBoundingClientRect().top -
+              websitesContainer.getBoundingClientRect().top -
+              300
+          ) < 100
+        ) {
+          document.body.classList.add("fadeOutToLeft");
+          setTimeout(() => {
+            window.location.href = ele.dataset.url;
+          }, 500);
+        }
+      });
+    });
   });
-});
